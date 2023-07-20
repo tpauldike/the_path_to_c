9188bb4970b19e14f7dea708eccbee55c8262dce
tpauldike <topman4loveworld@gmail.com>
2023-07-20 11:06:46 +0000

    Introduction to C programming on Linux

---
+++ intro/main.c
@@ -0,0 +1,30 @@
+/* Example of C program
+   I like to program in C
+   Indeed C is fun!!!
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int score = 100;
+
+	printf("I made %i%% in Maths\n", score);
+
+	// I made _____% in Maths
+/*
+	puts("I made \"100%\" in Maths");
+
+	putchar('I');
+	putchar(' ');
+	putchar('l');
+	putchar('o');
+	putchar('v');
+	putchar('e');
+	putchar(' ');
+	putchar('C');
+	putchar('\n');
+*/
+
+         return (0);
+}
