34457ee077c02b30d1bd91268106a12cfff10f9c
tpauldike <topman4loveworld@gmail.com>
2023-10-06 09:12:02 +0000

    session 2, for arrays, pointers and strings

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -2,7 +2,31 @@
 
 int main(void)
 {
+	int number = 123;
+	int idx;
+	int numbers[3] = {10, 2, 3};
+/*
+	printf("The size of number: %lu bytes\n", sizeof(number));
+	printf("The size of numbers: %lu bytes\n", sizeof(numbers));
+	printf("Value stored in numbers[0]: %d\n", numbers[0]);
+	printf("Value stored in numbers[1]: %d\n", numbers[1]);
+	printf("Value stored in numbers[2]: %d\n", numbers[2]);
 
+	printf("Value stored in number: %d\n", number);
 
+	numbers[1] = 10;
+	for (idx = 0; idx < 3; idx++)
+	{
+		printf("Value stored in numbers[%d]: %d\n", idx, numbers[idx]);
+	}
+*/
+//	printf("numbers: %p\n", numbers);
+	printf("(numbers + 2): %p\n", (numbers + 2));
+	printf("&numbers[2]: %p\n", &numbers[2]);
+	
+	printf("*(numbers + 2): %d\n", *(numbers + 2));
+	printf("*numbers + 2: %d\n", *numbers + 2);
+	printf("numbers[2]: %d\n", numbers[2]);
+	
 	return (0);
 }
---
+++ pointers/string.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char letters[4] = {'c', 'a', 'r', '\0'};
+	char letters2[4] = "car2";
+//	int numbers[3] = {1, 2, 3};
+
+	printf("letters: %s\n", letters);
+	printf("letters2: %s\n", letters2);
+	
+	return (0);
+}
+//@tpauldike
